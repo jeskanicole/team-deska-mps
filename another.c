70bac0eb4462a33ff52615f6a2d76efb9be45f19
jeskanicole <jeska_cabiles@yahoo.com>
2018-03-11 19:57:26 +0800

    142mp

--- another.c
+++ another.c
@@ -1,19 +1,32 @@
-//http://www.sanfoundry.com/c-program-tower-of-hanoi-using-recursion/
+// http://www.sanfoundry.com/c-program-tower-of-hanoi-using-recursion/
+// https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.2.0/com.ibm.zos.v2r2.bpxbd00/clock.htm
 
+#include <time.h>
 #include <stdio.h>
  
 void towers(int, char, char, char);
- 
-int main(){
+double time1, timedif; 
+
+int main(){   
+    time1 = (double) clock();            /* get initial time */
+    time1 = time1 / CLOCKS_PER_SEC;      /*    in seconds    */
+
     int num;
- 
     printf("Enter the number of disks : ");
     scanf("%d", &num);
     printf("The sequence of moves involved in the Tower of Hanoi are :\n");
     towers(num, 'A', 'C', 'B');
+
+
+    /* call clock a second time */
+    timedif = ( ((double) clock()) - time1)/ CLOCKS_PER_SEC;
+    printf("\nThe elapsed time is %f seconds\n\n", timedif);
+
+
     return 0;
 }
 void towers(int num, char frompeg, char topeg, char auxpeg){
+   
     if (num == 1){
         printf("\n Move disk 1 from peg %c to peg %c", frompeg, topeg);
         return;
